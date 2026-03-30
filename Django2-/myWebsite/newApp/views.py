1eddc1a18f354331ef5892ebdd5428969b3dfb5e
They_callme_starboi <rm710858@gmail.com>
2026-03-30 16:50:34 +0530

    Day 5

---
+++ Django2-/myWebsite/newApp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def app(request):
+    return render(request, 'newApp/app.html')
+
+def about(request):
+    return render(request, 'newApp/about.html')
+
+def contacts(request):
+    return render(request, 'newApp/contacts.html')
+
+# Create your views here.
+

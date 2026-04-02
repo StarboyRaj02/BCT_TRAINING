46dd7a43a42ffad53d48621379aeb32da3566534
They_callme_starboi <rm710858@gmail.com>
2026-04-02 16:59:50 +0530

    Day 9

--- Django2/myWebsite/myWebsite/views.py
+++ Django2/myWebsite/myWebsite/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from newApp.models import Product
 
 def home(request):
-    return render(request, 'index.html')
+    products = Product.objects.all()[:4]  # Get first 4 products for home page
+    return render(request, 'index.html', {'products': products})
 def about(request):
     return HttpResponse("About")
 def contact(request):
